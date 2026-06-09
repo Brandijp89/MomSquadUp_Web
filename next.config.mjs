d5bdab9ae49f24957d387149ec74e626218ed7ba
OPunga <ronnyopunga96@gmail.com>
2026-06-10 00:07:39 +0300

    momsquadweb

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // <=== Enables static exports
+  basePath: '/MomSquadUp_Web', // Required for GitHub Pages sub-directory hosting
+  images: {
+    unoptimized: true, // Required because Next.js Image Optimization needs a Node server
+  },
+};
 
 export default nextConfig;
